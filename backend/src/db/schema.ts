fff93a02aa891c49f71d287c4b507aa68a5300b7
nika131 <nmegrelishvili313@gmail.com>
2026-03-11 21:56:17 +0400

    initial setup

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,10 @@
+import type { varbinary } from "drizzle-orm/mysql-core";
+import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    fullName: text("full_name").notNull(),
+    email: varchar("email", {length: 256}).unique().notNull(),
+    hashedPassword: text("hashed_password").notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+})
